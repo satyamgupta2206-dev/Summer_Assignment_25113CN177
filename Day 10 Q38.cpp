b8167700f9296573c2e7ff2fb41b1182ee91cd62
satyamgupta2206-dev <satyamgupta2206@gmail.com>
2026-06-10 18:30:19 +0530

    Create Day 10 Q38.cpp

---
+++ Day 10 Q38.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+   
+    for (int i = rows; i >= 1; i--) {
+        
+       
+        for (int space = 0; space < rows - i; space++) {
+            cout << " ";
+        }
+
+      
+        for (int j = 1; j <= (2 * i - 1); j++) {
+            cout << "*";
+        }
+
+      
+        cout << endl;
+    }
+
+    return 0;
+}

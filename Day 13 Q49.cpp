f8107ccccfad5e84d74aa82a71a2efe6df49c3c6
satyamgupta2206-dev <satyamgupta2206@gmail.com>
2026-06-13 18:28:38 +0530

    Create Day 13 Q49.cpp

---
+++ Day 13 Q49.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int arr[5];
+
+   
+    cout << "Enter 5 numbers: " << endl;
+    for (int i = 0; i < 5; i++) {
+        cin >> arr[i];
+    }
+
+   
+    cout << "\nThe elements in the array are: " << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}

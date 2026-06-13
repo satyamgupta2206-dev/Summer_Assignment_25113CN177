9271571265d9db83014dd054e8fa13b8534b9547
satyamgupta2206-dev <satyamgupta2206@gmail.com>
2026-06-13 18:32:43 +0530

    Create Day 13 Q51.cpp

---
+++ Day 13 Q51.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+   
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+   
+    if (n <= 0) {
+        cout << "Invalid array size!" << endl;
+        return 1;
+    }
+
+    int arr[n];
+
+  
+    cout << "Enter " << n << " numbers: " << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+   
+    int largest = arr[0];
+    int smallest = arr[0];
+
+  
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i]; 
+        }
+        if (arr[i] < smallest) {
+            smallest = arr[i];
+        }
+    }
+
+   
+    cout << "Largest element: " << largest << endl;
+    cout << "Smallest element: " << smallest << endl;
+
+    return 0;
+}

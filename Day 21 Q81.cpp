e8897f934c73bca9491a7a7ff09b3f06bdc17742
satyamgupta2206-dev <satyamgupta2206@gmail.com>
2026-06-21 18:13:41 +0530

    Create Day 21 Q81.cpp

---
+++ Day 21 Q81.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char str[100];
+    int length = 0;
+
+    cout << "Enter a string: ";
+    cin.getline(str, 100); 
+
+   
+    while (str[length] != '\0') {
+        length++;
+    }
+
+    cout << "Length of the string is: " << length << endl;
+
+    return 0;
+}

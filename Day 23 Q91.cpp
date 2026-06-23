5d07c5a3cbfd05c01a0faae629ea6d4c6a125150
satyamgupta2206-dev <satyamgupta2206@gmail.com>
2026-06-23 19:24:31 +0530

    Create Day 23 Q91.cpp

---
+++ Day 23 Q91.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <algorithm> 
+
+using namespace std;
+
+
+bool checkAnagram(string str1, string str2) {
+  
+    if (str1.length() != str2.length()) {
+        return false;
+    }
+
+    
+    sort(str1.begin(), str1.end());
+    sort(str2.begin(), str2.end());
+
+   
+    return (str1 == str2);
+}
+
+int main() {
+    string s1, s2;
+
+   
+    cout << "Enter first string: ";
+    cin >> s1;
+    cout << "Enter second string: ";
+    cin >> s2;
+
+    
+    if (checkAnagram(s1, s2)) {
+        cout << "\"" << s1 << "\" and \"" << s2 << "\" are anagrams." << endl;
+    } else {
+        cout << "\"" << s1 << "\" and \"" << s2 << "\" are NOT anagrams." << endl;
+    }
+
+    return 0;
+}
